4f09828b4a6411f1145eebb45017df32f5a8f540
unknown <kausharma389@gmail.com>
2024-07-03 20:30:02 +0530

    laravel

---
+++ app/Http/Controllers/API/StudentController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Student;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::all();
+        return response()->json([
+            'status' => 200,
+            'student' => $students,
+        ]);
+    }
+    public function store(Request $request)
+    {
+        $student = new Student();
+        $student->name = $request->input('name');
+        $student->course = $request->input('course');
+        $student->email = $request->input('email');
+        $student->phone = $request->input('phone');
+        $student->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Student Added Successfully',
+        ]);
+    }
+    public function edit($id)
+    {
+        $student = Student::find($id);
+        return response()->json([
+            'status' => 200,
+            'student' => $student,
+        ]);
+    }
+    public function update(Request $request, $id)
+    {
+        $student = Student::find($id);
+        $student->name = $request->input('name');
+        $student->course = $request->input('course');
+        $student->email = $request->input('email');
+        $student->phone = $request->input('phone');
+        $student->update();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Student Updated Successfully',
+        ]);
+    }
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+        $student->delete();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Student Deleted Successfully',
+        ]);
+    }
+}
+
